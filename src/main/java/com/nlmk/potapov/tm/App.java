5af35cebc9d09e403fda8b4335690544c71fef2f
Потапов Вадим <koptec1987@mail.ru>
2020-07-15 00:00:54 +0300

    Произведена русификация и форматирование текста. Добавлена цикличная обработка терминальных команд. Добавлена терминальная команда "exit".

--- src/main/java/com/nlmk/potapov/tm/App.java
+++ src/main/java/com/nlmk/potapov/tm/App.java
@@ -1,5 +1,7 @@
 package com.nlmk.potapov.tm;
 
+import java.util.Scanner;
+
 import static com.nlmk.potapov.tm.constant.TerminalConst.*;
 
 public class App {
@@ -7,45 +9,74 @@ public class App {
     public static void main(final String[] args) {
         displayWelcome();
         run(args);
+        final Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while (!EXIT.equals(command)){
+            System.out.print("Task Manager> ");
+            command = scanner.nextLine();
+            run(command);
+        }
     }
 
     private static void run(final String[] args) {
         if (args == null) return;
         if (args.length < 1) return;
         final String param = args[0];
+        run(param);
+    }
+
+    private static int run(final String param) {
         switch (param) {
-            case HELP: displayHelp();
-            case VERSION: displayVersion();
-            case ABOUT: displayAbout();
-            default: displayError();
+            case HELP: return displayHelp();
+            case VERSION: return displayVersion();
+            case ABOUT: return displayAbout();
+            case EXIT: return displayExit();
+            default: return displayError();
         }
     }
 
-    private static void displayHelp() {
-        System.out.println("version - Display program version.");
-        System.out.println("about - Display developer info.");
-        System.out.println("help - Display list of terminal commands.");
-        System.exit(0);
+    private static int displayHelp() {
+        System.out.println(BLOCK_SEPARATOR);
+        System.out.println(INDENT+"version - Информация о версии приложения.");
+        System.out.println(INDENT+"about - Информация о разработчике.");
+        System.out.println(INDENT+"help - Вывод списка терминальных команд.");
+        System.out.println(INDENT+"exit - Выход из приложения.");
+        System.out.println(BLOCK_SEPARATOR);
+        return(0);
     }
 
-    private static void displayVersion() {
-        System.out.println("1.0.0");
-        System.exit(0);
+    private static int displayVersion() {
+        System.out.println(BLOCK_SEPARATOR);
+        System.out.println(INDENT+"1.0.0");
+        System.out.println(BLOCK_SEPARATOR);
+        return(0);
     }
 
-    private static void displayAbout() {
-        System.out.println("Vadim Potapov");
-        System.out.println("potapov_vs@nlmk.com");
-        System.exit(0);
+    private static int displayAbout() {
+        System.out.println(BLOCK_SEPARATOR);
+        System.out.println(INDENT+"Потапов Вадим");
+        System.out.println(INDENT+"potapov_vs@nlmk.com");
+        System.out.println(BLOCK_SEPARATOR);
+        return(0);
     }
 
-    private static void displayError() {
-        System.out.println("Error! Unknown program argument...");
-        System.exit(-1);
+    private static int displayError() {
+        System.out.println(BLOCK_SEPARATOR);
+        System.out.println(INDENT+"Ошибка! Неизвестная терминальная команда...");
+        System.out.println(BLOCK_SEPARATOR);
+        return(-1);
+    }
+
+    private static int displayExit() {
+        System.out.println(BLOCK_SEPARATOR);
+        System.out.println(INDENT+"Завершение работы приложения...");
+        System.out.println(INDENT+"Всего хорошего.");
+        return(0);
     }
 
     private static void displayWelcome() {
-        System.out.println("** WELCOME TO TASK MANAGER **");
+        System.out.println("** ДОБРО ПОЖАЛОВАТЬ В TASK MANAGER **");
+        System.out.println(BLOCK_SEPARATOR);
     }
 
 }
--- src/main/java/com/nlmk/potapov/tm/constant/TerminalConst.java
+++ src/main/java/com/nlmk/potapov/tm/constant/TerminalConst.java
@@ -5,5 +5,8 @@ public final class TerminalConst {
     public static final String HELP = "help";
     public static final String VERSION = "version";
     public static final String ABOUT = "about";
+    public static final String EXIT = "exit";
+    public static final String BLOCK_SEPARATOR = "-----------------------------------------------";
+    public static final String INDENT = "   ";
 
 }
