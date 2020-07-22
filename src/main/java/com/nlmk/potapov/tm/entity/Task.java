2725ba2afdf2c8fd1b11f3e54758a4ac9d4fd377
Потапов Вадим <koptec1987@mail.ru>
2020-07-22 21:53:30 +0300

    Добавлены классы Project и Task.

---
+++ src/main/java/com/nlmk/potapov/tm/entity/Task.java
@@ -0,0 +1,47 @@
+package com.nlmk.potapov.tm.entity;
+
+public class Task {
+
+    private Long id = System.nanoTime();
+
+    private String name = "";
+
+    private String description = "";
+
+    public Task() {
+    }
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return id + " : " + name;
+    }
+
+}
