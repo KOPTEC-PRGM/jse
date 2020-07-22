de9db076c18012b64457e017e86afb1ea352b90a
Потапов Вадим <koptec1987@mail.ru>
2020-07-22 22:05:55 +0300

    Добавлены константы в TerminalConst.

--- src/main/java/com/nlmk/potapov/tm/constant/TerminalConst.java
+++ src/main/java/com/nlmk/potapov/tm/constant/TerminalConst.java
@@ -6,7 +6,17 @@ public final class TerminalConst {
     public static final String VERSION = "version";
     public static final String ABOUT = "about";
     public static final String EXIT = "exit";
+
     public static final String BLOCK_SEPARATOR = "-----------------------------------------------";
     public static final String INDENT = "   ";
+    public static final String INPUT_MESSAGE = "Task Manager> ";
+
+    public static final String PROJECT_CREATE = "project-create";
+    public static final String PROJECT_CLEAR = "project-clear";
+    public static final String PROJECT_LIST = "project-list";
+
+    public static final String TASK_CREATE = "task-create";
+    public static final String TASK_CLEAR = "task-clear";
+    public static final String TASK_LIST = "task-list";
 
 }
