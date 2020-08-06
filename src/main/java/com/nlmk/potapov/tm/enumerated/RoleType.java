ca1bc8e1ad39341a7b9761d19f94fe5457efc6e0
Потапов Вадим <koptec1987@mail.ru>
2020-08-06 11:29:25 +0300

    Добавлено поле displayName для класса RoleType.

--- src/main/java/com/nlmk/potapov/tm/enumerated/RoleType.java
+++ src/main/java/com/nlmk/potapov/tm/enumerated/RoleType.java
@@ -2,7 +2,22 @@ package com.nlmk.potapov.tm.enumerated;
 
 public enum RoleType {
 
-    USER,
-    ADMIN
+    USER("Пользователь"),
+    ADMIN("Администратор");
+
+    private final String displayName;
+
+    RoleType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 
 }
